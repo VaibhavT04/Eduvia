648e7d6062fa7596a1a08d031435966cddc6d31a
Sandesh30-cloud <138008452+Sandesh30-cloud@users.noreply.github.com>
2025-03-30 19:22:34 +0530

    update

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
+  dialect: "postgresql",
   schema: "./configs/schema.js",
-  out: "./drizzle",
-  dialect: "pg",
-  driver: "pg", // Try using "postgresql" instead of "pg" or "pglite"
+  // out: "./drizzle",
+  // driver: "pglite",
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_w38ZYdhzISmL@ep-lingering-salad-a8l51swm-pooler.eastus2.azure.neon.tech/ai-study-material?sslmode=require",
+    // Use "url" here as Drizzle expects:
+    url: 'postgresql://neondb_owner:npg_w38ZYdhzISmL@ep-lingering-salad-a8l51swm-pooler.eastus2.azure.neon.tech/ai-study-material?sslmode=require',
   },
-})
+});
 
 
 
