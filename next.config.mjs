7383d23d4e930d97cc7e1d147b4a59d112f34002
Vaibhav <vaibhavtatkare04@gmail.com>
2025-04-25 13:14:17 +0530

    Inngest deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   env: {
     DATABASE_CONNECTION_STRING: process.env.DATABASE_CONNECTION_STRING,
   },
+  experimental: {
+    appDir: true, // ensures app directory routing
+    serverActions: true // only if you're using server actions
+  },
 };
 
 export default nextConfig;
